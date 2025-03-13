6b630cb20be3e2e6c9eada981b6c236d24b5aae1
ledetav <nord.hilman@gmail.com>
2025-03-13 14:46:25 +0300

    fix: fix names in database models

--- NWA/NWA_archive/models.py
+++ NWA/NWA_archive/models.py
@@ -1,53 +1,45 @@
 from django.db import models
 
-class moderators(models.Model):
-    position = models.CharField(max_length=30)
-    shedule = models.CharField(max_length=30)
+class Moderators(models.Model):
+    moderator_position = models.CharField(max_length=100)
+    moderator_shedule = models.CharField(max_length=100)
 
+class NorthernWarmers(models.Model):
+    nw_name = models.CharField(max_length=30)
+    nw_join_date = models.DateField()
 
-class northern_warmers(models.Model):
-    date = models.DateField()
-    name = models.CharField(max_length=30)
+class Blogs(models.Model):
+    moderator_id = models.ForeignKey(Moderators, on_delete=models.CASCADE)
+    celebrant_name = models.CharField(max_length=30)
+    birthday_date = models.DateField()
 
-
-class blogs(models.Model):
-    boss_id = models.ForeignKey(moderators, on_delete = models.CASCADE)
-    name = models.CharField(max_length=30)
-    date = models.DateField()
-
-
-class arts(models.Model):
-    nw_ID = models.ForeignKey(northern_warmers, on_delete = models.CASCADE)
-    art = models.CharField(max_length=100)
-    date = models.DateField()
-    blog_id = models.ForeignKey(blogs, on_delete = models.CASCADE)
-    type = models.IntegerField()
+class NWAArchiveArts(models.Model):
+    nw_ID = models.ForeignKey(NorthernWarmers, on_delete=models.CASCADE)
+    work_art = models.CharField(max_length=100)
+    work_publication_date = models.DateField()
+    work_type = models.IntegerField()
     blog_part = models.IntegerField()
+    blog_id = models.ForeignKey(Blogs, on_delete=models.CASCADE)
 
-
-class text(models.Model):
-    nw_ID = models.ForeignKey(northern_warmers, on_delete = models.CASCADE)
-    texts = models.CharField(max_length=100)
-    date = models.DateField()
-    boss_id = models.ForeignKey(moderators, on_delete = models.CASCADE)
-    blog_id = models.ForeignKey(blogs, on_delete = models.CASCADE)
-    type = models.IntegerField()
+class NWAArchiveTexts(models.Model):
+    nw_ID = models.ForeignKey(NorthernWarmers, on_delete=models.CASCADE)
+    work_text = models.TextField()
+    work_publication_date = models.DateField()
+    work_type = models.IntegerField()
     blog_part = models.IntegerField()
+    blog_id = models.ForeignKey(Blogs, on_delete=models.CASCADE)
+    moderator_id = models.ForeignKey(Moderators, on_delete=models.CASCADE)
 
-
-class code(models.Model):
-    nw_ID = models.ForeignKey(northern_warmers, on_delete = models.CASCADE)
-    codes =models.CharField(max_length=100)
-    date = models.DateField()
-    boss_id = models.ForeignKey(moderators, on_delete = models.CASCADE)
-    blog_id = models.ForeignKey(blogs, on_delete = models.CASCADE)
-
-
-class congratulations(models.Model):
-    nw_ID = models.ForeignKey(northern_warmers, on_delete = models.CASCADE)
-    texts = models.CharField(max_length=100)
-    sender_ID = models.SET_NULL
+class NWAArchiveCongratulations(models.Model):
+    nw_ID = models.ForeignKey(NorthernWarmers, on_delete=models.CASCADE)
+    congratulation_text = models.CharField(max_length=100)
     sender_name = models.CharField(max_length=30)
-    image_url = models.SET_NULL
-    date = models.DateField()
-    blog_id = models.ForeignKey(blogs, on_delete = models.CASCADE)
+    work_publication_date = models.DateField()
+    blog_id = models.ForeignKey(Blogs, on_delete=models.CASCADE)
+
+class NWAArchiveCodes(models.Model):
+    nw_ID = models.ForeignKey(NorthernWarmers, on_delete=models.CASCADE)
+    work_code = models.CharField(max_length=100)
+    work_publication_date = models.DateField()
+    blog_id = models.ForeignKey(Blogs, on_delete=models.CASCADE)
+    moderator_id = models.ForeignKey(Moderators, on_delete=models.CASCADE)
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # NWA
+
 Northern Warmers' Archive (Архив Северный Дарителей Тепла)
 
 📍 [Ссылка на концепт-документ проекта](https://docs.google.com/document/d/1cf031_YaoTwOP2QCFb3lFGhysR7RV6dJEYEIs5O4idU/edit?tab=t.0)
