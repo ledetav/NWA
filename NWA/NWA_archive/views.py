fbd079e66f07e6c4237026cdda4e0b7a1161ea16
Zoya2502 <zoe.shumeiko@gmail.com>
2025-05-17 20:06:09 +0300

    feat: Add templates directory

--- NWA/NWA_archive/views.py
+++ NWA/NWA_archive/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Главная страница")
+
+def log(request):
+    return HttpResponse("Лог")
