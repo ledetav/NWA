a5ead66bfbcf8d9aba92ef32970c416c21eac7d1
ledetav <zoe.shumeiko@gmail.com>
2025-05-20 00:15:45 +0300

    refactor: Refactor URL routing and views to implement main and test page views

--- NWA/NWA_archive/views.py
+++ NWA/NWA_archive/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request):
+def main_page_view(request):
     #data = { 'здесь будут переменные'} как передавать переменные: "<название переменное>": <переменная>
     return render(request, 'index.html')
 
-def log(request):
+def log_page_view(request):
     return render(request, 'log.html')
+
+def test_page_view(request):
+    return render(request, 'test.html')
