b9043890d7f23521011a82d7c90fca4291a9264b
Zoya2502 <zoe.shumeiko@gmail.com>
2025-05-19 21:33:23 +0300

    feat: Add initial HTML structure

--- NWA/NWA_archive/views.py
+++ NWA/NWA_archive/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    data = { 'здесь будут переменные'} #как передавать переменные: "<название переменное>": <переменная>
-    return render(request, 'index.html', context=data)
+    #data = { 'здесь будут переменные'} как передавать переменные: "<название переменное>": <переменная>
+    return render(request, 'index.html')
 
 def log(request):
     return render(request, 'log.html')
