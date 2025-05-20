a9fdd49a327a83f6b5f0b0040cda5cdb7881ecc5
Zoya2502 <zoe.shumeiko@gmail.com>
2025-05-20 20:50:19 +0300

    feat: Implement basic calendar layout with grid

--- NWA/NWA_archive/views.py
+++ NWA/NWA_archive/views.py
@@ -4,10 +4,10 @@ from .models import Moderators, NorthernWarmers, Blogs, NWAArchiveArts, NWAArchi
 from django.db import connection
 
 def index(request):
-    blog = Blogs.objects.all()
-    sum_blogs = len(blog)
-    data = {"sum_blogs":"sum_blogs"}
-    return render(request, 'index.html', context = data)
+    # blog = Blogs.objects.all()
+    # sum_blogs = len(blog)
+    # data = {"sum_blogs":"sum_blogs"}
+    return render(request, 'index.html')
 
 def log(request):
     return render(request, 'log.html')
