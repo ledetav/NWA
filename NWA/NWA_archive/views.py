0e4b5509fbd70d26cb360b4933d4fe56ca2369d3
Zoya2502 <zoe.shumeiko@gmail.com>
2025-05-21 23:32:18 +0300

    Feat: Display moderator info on main page

--- NWA/NWA_archive/views.py
+++ NWA/NWA_archive/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Moderators, NorthernWarmers, Blogs, NWAArchiveArts, NWAArchiveTexts, NWAArchiveCongratulations, NWAArchiveCodes
 from django.db import connection
 
@@ -8,7 +8,28 @@ def main_page_view(request):
     art_count = NWAArchiveArts.objects.count()  # Получаем количество строк
     text_count = NWAArchiveTexts.objects.count()  # Получаем количество строк
     code_count = NWAArchiveCodes.objects.count()  # Получаем количество строк
-    context = {'blog_count': blog_count, 'art_count': art_count, 'text_count': text_count, 'code_count': code_count}
+    moderator_id = request.GET.get('moderator_id')  # Получаем id модератора из GET-параметра
+    
+    if moderator_id:
+        # Используем get_object_or_404 для получения модератора или 404 ошибки
+        moderator = get_object_or_404(Moderators, pk=moderator_id)
+        name = moderator.nw_ID.nw_name
+        position = moderator.moderator_position
+    else:
+        moderator_id = 1
+        moderator = Moderators.objects.first()
+        name = moderator.nw_ID.nw_name
+        position = moderator.moderator_position
+
+    context = {
+        'blog_count': blog_count,
+        'art_count': art_count,
+        'text_count': text_count,
+        'code_count': code_count,
+        'name': name,
+        'position': position,
+        'moderator_id': moderator_id
+    }
     return render(request, 'index.html', context)
 
 def log_page_view(request):
